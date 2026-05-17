1644db129f96a31c5d2513e8faab37c961469c5f
Cory <cness@ctr-electronics.com>
2026-05-17 17:14:20 -0400

    Add manual PID tuning with TorqueCurrentFOC page (#161)
    Co-authored-by: Ozrien <ozrien@ctr-electronics.com>
    Co-authored-by: Benjamin Hall <bhall@ctr-electronics.com>

---
+++ source/_extensions/controls_js_sim/plant/turret-plant.js
@@ -0,0 +1,89 @@
+class TurretPlant {
+    constructor(TimestepS, highInertia) {
+        this.TimestepS = TimestepS;
+
+        // Constants related to plant model
+        // Turret
+
+        let mass = 10; // Turret wheel mass in Kg
+        let radius = 0.2032; // 8 inch radius, converted to meters
+
+        // Gearbox
+        this.GEARBOX_RATIO = 1.0 / 20.0; // output over input - 1:20 reduction gear ratio
+        if (highInertia) {
+            this.GEARBOX_RATIO = 1.0 / 1.0;
+        }
+
+        // Kraken FOC Torque-Constant
+        let Kt = 0.01981; // Nm/A torque constant -  Taken from Kraken Motor Performance Analysis Report
+
+        // TODO: better comment and descriptive variable naming
+        // Constants from the blog post equations
+        this.C1 = 2 * Kt / (mass * radius * radius * this.GEARBOX_RATIO);
+        this.C3 = 2 / (mass * radius * radius);
+
+        this.systemNoise = false;
+        // Simulate 3 amp std dev system noise at the loop update frequency
+        this.gaussianNoise = gaussian(0, 3);
+    }
+    init() {
+        this.speed = 0;
+        this.curpositionRev = 0;
+    }
+
+    restrict(inAmps, supplyV) {
+        // restrict output current based on supply voltage and back EMF
+        let Rc = 0.0252; // Coil & Wiring Resistance in Ohms
+        let maxRps = 96.4; // Max motor velocity in RPS
+
+        let bemf = this.getCurrentSpeedRPS() / this.GEARBOX_RATIO * supplyV / maxRps;
+        let maxCurrent = (supplyV - bemf) / Rc;
+        let minCurrent = (-supplyV - bemf) / Rc;
+        if (inAmps > maxCurrent) {
+            inAmps = maxCurrent;
+        } else if (inAmps < minCurrent) {
+            inAmps = minCurrent;
+        }
+
+        return inAmps;
+    }
+
+    update(inAmps) {
+        // Simulate friction - both static and dynamic
+        let extTrq = 0.025/this.GEARBOX_RATIO; // 0.025 * reduction Nm of static friction
+        extTrq += 0.0005*this.speed; // 0.0005 Nm of friction for every rad/s it's spinning
+
+        // Increase friction when not moving to simulate static friction
+        if (this.speedPrev == 0) {
+            extTrq = 0.03/this.GEARBOX_RATIO;
+        }
+
+        // Simulate system noise only if control input is outside 3 amps or speed is high
+        if (this.systemNoise && (Math.abs(inAmps) > 3 || this.getCurrentSpeedRPS() > 1)) {
+            // apply system noise
+            inAmps += this.gaussianNoise();
+        }
+
+        // Simulate main Plant behavior
+        this.speed = (this.TimestepS*this.C1*inAmps + this.speed);
+        let speedsign = Math.sign(this.speed);
+        this.speed -= speedsign * this.TimestepS*this.C3*extTrq;
+        if (Math.sign(this.speed) != speedsign) {
+            this.speed = 0;
+        }
+
+        this.curpositionRev += this.speed / 2.0 / Math.PI * this.TimestepS;
+    }
+
+    getCurrentSpeedRPS() {
+        return this.speed/2/Math.PI;
+    }
+
+    getCurrentPosition() {
+        return this.curpositionRev;
+    }
+
+    setSystemNoise(enabled) {
+        this.systemNoise = enabled;
+    }
+}
--- source/docs/application-notes/devblog.rst
+++ source/docs/application-notes/devblog.rst
@@ -8,6 +8,7 @@ Welcome to the development blog. Here, we will highlight various features of CTR
 - :doc:`canbus-latency-and-frequency`
 - :doc:`tuner-evolution`
 - :doc:`update-frequency-impact`
+- :doc:`manual-pid-tuning`
 - :doc:`tuning-canrange`
 
 .. toctree::
@@ -17,4 +18,5 @@ Welcome to the development blog. Here, we will highlight various features of CTR
    canbus-latency-and-frequency
    tuner-evolution
    update-frequency-impact
+   manual-pid-tuning
    tuning-canrange
